9d248b3c746834b705b6fdb05e9194fa42321c26
gaoyang <785175323@qq.com>
2023-07-24 11:39:32 +0800

    新增滚动条

--- src/main/kotlin/SearchResult.kt
+++ src/main/kotlin/SearchResult.kt
@@ -1,12 +1,14 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -14,55 +16,70 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import sqlite.ExcelDataPo
 
+@OptIn(ExperimentalFoundationApi::class)
 @Preview
 @Composable
 fun searchResult(searchResult: SnapshotStateList<ExcelDataPo>, searchText: MutableState<String>) {
 
-    Column(
-        Modifier
-            .fillMaxWidth()
-            .fillMaxHeight()
-            .padding(5.dp)
-            .border(border = BorderStroke(1.dp, Color.Black))
-            .background(Color.White)
-    ) {
-        if (searchResult.isNotEmpty()) {
-            //进行了点击查询
-            searchResult.forEach({
-                Row {
-                    it.rowIndex?.let { it ->
+    val state = rememberLazyListState()
 
-                        Text(
-                            text = "所在行" + it.toString(),
-                            Modifier.padding(start = 3.dp, bottom = 3.dp).width(80.dp)
-                                .border(BorderStroke(1.dp, Color.Black)),
-                            maxLines = 1,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                    it.columnIndex?.let { it ->
-                        Text(
-                            text = "所在列" + it.toString(),
-                            Modifier.padding(start = 3.dp, bottom = 3.dp).width(80.dp)
-                                .border(BorderStroke(1.dp, Color.Black)),
-                            maxLines = 1,
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                    it.data?.let { it ->
-                        Text(
-                            text = it,
-                            Modifier.padding(start = 3.dp, bottom = 3.dp).width(300.dp)
-                                .border(BorderStroke(1.dp, Color.Black)),
-                            maxLines = 1,
-                            overflow = TextOverflow.Ellipsis,
-                            textAlign = TextAlign.Center
-                        )
-                    }
+    Box(
+        modifier = Modifier.fillMaxSize()
+            .background(color = Color.White)
+    ) {
+
+        LazyColumn(
+            Modifier
+                .fillMaxSize()
+                .padding(5.dp)
+                .border(border = BorderStroke(1.dp, Color.Black))
+                .background(Color.White).padding(end = 12.dp), state
+        ) {
+            items(searchResult) { result ->
+                run {
+                    showResult(result)
                 }
-            })
-        } else {
-            Text("暂无搜索内容~")
+            }
+        }
+
+        VerticalScrollbar(
+            modifier = Modifier.align(Alignment.CenterEnd).width(35.dp).fillMaxHeight(0.9f).padding(end = 20.dp),
+            adapter = rememberScrollbarAdapter(scrollState = state)
+        )
+    }
+}
+
+@Composable
+fun showResult(result: ExcelDataPo) {
+    //进行了点击查询
+    Row {
+        result.rowIndex?.let { it ->
+            Text(
+                text = "所在行" + it.toString(),
+                Modifier.padding(start = 3.dp, bottom = 3.dp).width(80.dp)
+                    .border(BorderStroke(1.dp, Color.Black)),
+                maxLines = 1,
+                textAlign = TextAlign.Center
+            )
+        }
+        result.columnIndex?.let { it ->
+            Text(
+                text = "所在列" + it.toString(),
+                Modifier.padding(start = 3.dp, bottom = 3.dp).width(80.dp)
+                    .border(BorderStroke(1.dp, Color.Black)),
+                maxLines = 1,
+                textAlign = TextAlign.Center
+            )
+        }
+        result.data?.let { it ->
+            Text(
+                text = it,
+                Modifier.padding(start = 3.dp, bottom = 3.dp).width(300.dp)
+                    .border(BorderStroke(1.dp, Color.Black)),
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
